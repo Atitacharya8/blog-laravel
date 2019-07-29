ca591fef10910a0083ea83d41b400c6acdcca452
atitacharya8 <atitacharya8@gmail.com>
2019-07-30 01:14:39 +0545

    this is my first blogging system in laravel

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+   @section('content')
+   <div class="jumbotron">
+         <h1 class="display-4">Welcome to Laravel!</h1>
+         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+         <hr class="my-4">
+         <p>
+         <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-primary btn-lg" href="/register" role="button">Register</a>
+      </p>
+      </div>  
+   @endsection
